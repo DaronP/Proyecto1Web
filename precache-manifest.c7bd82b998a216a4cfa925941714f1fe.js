d8868192da53dba4fcd4557c914d69621d5346e8
DaronP <per16362@uvg.edu.gt>
2019-09-30 12:55:17 -0600

    Proyecto

---
+++ precache-manifest.c7bd82b998a216a4cfa925941714f1fe.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2d3b7d76c268f2a3b13c5e0c5bca6d96",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1321df234b49c558bdee",
+    "url": "/static/css/main.ff60250d.chunk.css"
+  },
+  {
+    "revision": "d6def4d2bd713d0c14d2",
+    "url": "/static/js/2.3a667ea8.chunk.js"
+  },
+  {
+    "revision": "1321df234b49c558bdee",
+    "url": "/static/js/main.9dafac8a.chunk.js"
+  },
+  {
+    "revision": "9991c8f77bd3592b9932",
+    "url": "/static/js/runtime-main.7a36a96d.js"
+  },
+  {
+    "revision": "db2f988aee37c2a4efddc13069a4d0cc",
+    "url": "/static/media/bg.db2f988a.JPG"
+  },
+  {
+    "revision": "a18d4c225798a6edc49c1fcacc2e1205",
+    "url": "/static/media/header.a18d4c22.png"
+  }
+]);
